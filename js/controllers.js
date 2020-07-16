6e94778a349f628144a5f749108a2b1db0271157
anneloremaes <annelore.maes1989@gmail.com>
2020-07-16 14:55:20 +0200

    image as extra

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
 var restaurantApp = angular.module('restaurantApp', []);
 
 restaurantApp.controller('RestaurantTableController', ['$scope', function($scope) {
-    $scope.directory = [{ name: 'd Akkerhoeve', cuisine: 'BBQ', city: 'Lo-Reninge' },
-        { name: 'La kantina', cuisine: 'Salads, burgers, ...', city: 'Houthulst' },
-        { name: 'Il Trullo', cuisine: 'Italian', city: 'Roeselare' },
-        { name: 'de vlerke en de bille', cuisine: 'chicken', city: 'Ieper' }
+    $scope.directory = [{ name: 'd Akkerhoeve', cuisine: 'BBQ', city: 'Lo-Reninge', image: 'ribben' },
+        { name: 'La kantina', cuisine: 'Salads, burgers, ...', city: 'Houthulst', image: 'burger' },
+        { name: 'Il Trullo', cuisine: 'Italian', city: 'Roeselare', image: 'pizza' },
+        { name: 'de vlerke & de bille', cuisine: 'Chicken', city: 'Ieper', image: 'chickens' }
     ];
 
     $scope.selectRestaurant = function(row) {
